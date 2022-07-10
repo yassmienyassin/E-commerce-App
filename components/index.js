ee76a4df0863cd10d8f2d27c1a57884bf2319889
yassmienyassin <yassmien.yassin96@gmail.com>
2022-07-10 13:40:53 +0200

    add pages

---
+++ components/index.js
@@ -0,0 +1,7 @@
+import {default as Footer} from './Footer'
+import {default as Navbar} from './Navbar'
+import {default as Product} from './Product'
+import {default as Cart} from './Cart'
+import {default as HeroBanner} from './HeroBanner'
+import {default as FooterBanner} from './FooterBanner'
+import {default as Layout} from './Layout'
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import react from 'react';
+import { Toaster } from 'react-hot-toast';
+import Layout from '../components/Layout';
+import '../styles/globals.css';
+import {StateContext} from '../context/StateContext';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+    <Toaster />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  )
 }
 
 export default MyApp
